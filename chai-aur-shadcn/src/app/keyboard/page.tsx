df30148b581c4e4cf652da2187656f042ffb77d9
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-09-10 02:09:03 +0530

    add custom keyboard #2 with improved code structure over previous keyboard

--- chai-aur-shadcn/src/app/keyboard/page.tsx
+++ chai-aur-shadcn/src/app/keyboard/page.tsx
@@ -1,13 +1,18 @@
-import { Keyboard } from "@/components/keyboard";
+import { Keyboard, Keyboard2 } from "@/components/keyboard";
 import React from "react";
 
 function page() {
   return (
     <div className="">
       <div className="flex flex-col justify-center h-full mx-auto max-w-3xl rounded-md">
-        <h3 className="text-center">My custom Keyboard</h3>
+        <h3 className="text-center">My Custom Keyboard</h3>
         <div className="flex mx-auto items-center justify-center text-white">
           <Keyboard />
+        </div>
+        <h3 className="text-center">My Custom Keyboard #2</h3>
+
+        <div className="flex mx-auto items-center justify-center text-white">
+          <Keyboard2 />
         </div>
       </div>
     </div>
---
+++ chai-aur-shadcn/src/components/keyboard/index.ts
@@ -0,0 +1,4 @@
+import { Keyboard } from "./keyboard";
+import { Keyboard2 } from "./keyboard-2";
+
+export { Keyboard, Keyboard2 };
